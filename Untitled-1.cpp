137c9f138109ddb3b69e8b7606e1748c140b1dff
qmkjkj <111560931+qmkjkj@users.noreply.github.com>
2025-05-28 15:41:05 +0800

    第一次测试

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namesapce std;
+
+int n,m;
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+    cout<<"This is a first test"<<endl;
+    return 0;
+    
+}
