66877192a20ff87c7cb8c393d21c0c2e68ff7835
qmkjkj <111560931+qmkjkj@users.noreply.github.com>
2025-05-29 16:20:28 +0800

    保存

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,13 +1,61 @@
 #include<bits/stdc++.h>
-using namesapce std;
+#define int long long
+using namespace std;
 
-int n,m;
-
+int n,m,T;
+struct node{
+    int x,y,i;
+    bool operator <(const node &s)const{
+        return (s.x+s.y)>(x+y);
+    }
+};
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
+    cin>>T;
+    while(T--)
+    {
+        cin>>n;
+        int xx,yy,ans=0;
+        vector<node> a;
+        vector<int> x;
+        vector<int> y;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>xx>>yy;
+            a.push_back({xx,yy,i});
+            x.push_back(xx);
+            y.push_back(yy);
 
-    cout<<"This is a first test"<<endl;
-    return 0;
-    
+        }
+        if(n==1)
+        {
+            cout<<1<<endl;
+            continue;
+        }
+        sort(x.begin(),x.end());
+        sort(y.begin(),y.end());
+        for(int i=0;i<n;i++)
+        {
+            int tempx1=x[n-1];
+            int tempx2=x[0];
+            int tempy1=y[n-1];
+            int tempy2=y[0];
+
+            if(tempx1==a[i].x) tempx1=x[n-2];
+            if(tempx2==a[i].x) tempx2=x[1];
+            if(tempy1==a[i].y) tempy1=y[n-2];
+            if(tempy2==a[i].y) tempy2=y[1];
+            int tempans=(tempx1+1-tempx2)*(tempy1+1-tempy2);
+           // cout<<x[0]<<" "<<y[0]<<endl;
+            while(tempans<n)
+            {
+                tempans+=min((tempx1+1-tempx2),(tempy1+1-tempy2));
+            }
+            if(ans==0) ans=tempans;
+            else ans=min(ans,tempans);
+        }
+        cout<<ans<<endl;
+    }
 }
+    
